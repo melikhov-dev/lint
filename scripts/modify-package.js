9ea0d07cba75b0ef736477280ce92528726e2e5a
3y3 <3y3@ya.ru>
2024-08-11 02:20:34 +0300

    fix: Fix install script

--- scripts/modify-package.js
+++ scripts/modify-package.js
@@ -8,20 +8,20 @@ try {
     throw 'Unable to modify ' + filename;
 }
 
-function configure(command, impl) {
+function configure(command, impl, strict = true) {
     if (pkg.scripts[command]) {
-        if (pkg.scripts[command] !== impl) {
+        if (pkg.scripts[command] !== impl && strict) {
             throw `Lint command '${command}' already configured with different program`;
         }
     } else {
         pkg.scripts[command] = impl;
-        console.log('=> Add', command, 'script');
+        console.log('[@diplodoc/lint]', '=> Add', command, 'script');
     }
 }
 
 configure('lint', 'lint init && lint');
 configure('lint:fix', 'lint init && lint fix');
 configure('pre-commit', 'lint init && lint-staged');
-configure('prepare', 'husky install || true');
+configure('prepare', 'husky install || true', false);
 
 writeFileSync(filename, JSON.stringify(pkg, null, 2), 'utf8');
